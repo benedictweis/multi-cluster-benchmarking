133411bbea71a99b7db7740c17f335f89d6b879c
Benedict Weis <47110382+benedictweis@users.noreply.github.com>
2025-07-07 08:51:32 +0200

    fixes

--- docker.sh
+++ docker.sh
@@ -9,6 +9,8 @@ docker run -it \
 DATE=$(date +%Y%m%d%H%M%S)
 mkdir -p "./$RESULTS_DIR/$DATE"
 docker cp "mbench:/multi-cluster-benchmarking/$RESULTS_DIR/." "./$RESULTS_DIR/$DATE"
+docker cp "mbench:/multi-cluster-benchmarking/$CONTEXT_1_FILE" "./$CONTEXT_1_FILE"
+docker cp "mbench:/multi-cluster-benchmarking/$CONTEXT_2_FILE" "./$CONTEXT_2_FILE"
 docker cp "mbench:/multi-cluster-benchmarking/$KUBECONFIG_FILE" "./$KUBECONFIG_FILE"
 if [ -z "$(ls -A "./$RESULTS_DIR/$DATE")" ]; then
     rm -rf "./$RESULTS_DIR/$DATE"

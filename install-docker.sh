68d4849f71759ad0a7958ffd88e3fdff0660ea81
Benedict Weis <47110382+benedictweis@users.noreply.github.com>
2025-07-07 08:07:53 +0200

    added inotify increases to docker installation script

--- install-docker.sh
+++ install-docker.sh
@@ -13,3 +13,9 @@ echo \
 sudo apt-get update
 
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo sysctl fs.inotify.max_user_watches=524288
+sudo sysctl fs.inotify.max_user_instances=512
+
+echo "fs.inotify.max_user_watches = 524288" | sudo tee -a /etc/sysctl.conf
+echo "fs.inotify.max_user_instances = 512" | sudo tee -a /etc/sysctl.conf

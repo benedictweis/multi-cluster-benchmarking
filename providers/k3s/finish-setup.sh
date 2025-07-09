a9cdb63a0d6d043b003e74a12e492d32630d83ee
Benedict Weis <47110382+benedictweis@users.noreply.github.com>
2025-07-09 16:38:00 +0200

    yq fix

--- providers/k3s/finish-setup.sh
+++ providers/k3s/finish-setup.sh
@@ -13,25 +13,25 @@ KUBECONFIG_FILE_CLUSTER_2="../../kubeconfig_instance_2.yaml"
 echo $CLUSTER_1_NAME >"../../$CONTEXT_1_FILE"
 echo $CLUSTER_2_NAME >"../../$CONTEXT_2_FILE"
 
-yq -i "
+yq -y -i '
     .clusters[] |= 
-        if .name == \"default\" then .name = \"$CLUSTER_1_NAME\" | .cluster else . end |
+        if .name == "default" then .name = env.CLUSTER_1_NAME | .cluster else . end |
     .contexts[] |= 
-        if .name == \"default\" then .name = \"$CLUSTER_1_NAME\" | .context.cluster = \"$CLUSTER_1_NAME\" | .context.user = \"user-1\" else . end |
+        if .name == "default" then .name = env.CLUSTER_1_NAME | .context.cluster = env.CLUSTER_1_NAME | .context.user = "user-1" else . end |
     .users[] |= 
-        if .name == \"default\" then .name = \"user-1\" else . end |
-    .current-context = \"$CLUSTER_1_NAME\"
-" "$KUBECONFIG_FILE_CLUSTER_1"
+        if .name == "default" then .name = "user-1" else . end |
+    .current-context = env.CLUSTER_1_NAME
+' "$KUBECONFIG_FILE_CLUSTER_1"
 
-yq -i "
+yq -y -i '
     .clusters[] |= 
-        if .name == \"default\" then .name = \"$CLUSTER_2_NAME\" | .cluster else . end |
+        if .name == "default" then .name = env.CLUSTER_2_NAME | .cluster else . end |
     .contexts[] |= 
-        if .name == \"default\" then .name = \"$CLUSTER_2_NAME\" | .context.cluster = \"$CLUSTER_2_NAME\" | .context.user = \"user-2\" else . end |
+        if .name == "default" then .name = env.CLUSTER_2_NAME | .context.cluster = env.CLUSTER_2_NAME | .context.user = "user-2" else . end |
     .users[] |= 
-        if .name == \"default\" then .name = \"user-2\" else . end |
-    .current-context = \"$CLUSTER_2_NAME\"
-" "$KUBECONFIG_FILE_CLUSTER_2"
+        if .name == "default" then .name = "user-2" else . end |
+    .current-context = env.CLUSTER_2_NAME
+' "$KUBECONFIG_FILE_CLUSTER_2"
 
 yq -i ".clusters[0].name = \"$CLUSTER_1_NAME\"" "$KUBECONFIG_FILE_CLUSTER_1"
 yq -i ".clusters[0].name = \"$CLUSTER_2_NAME\"" "$KUBECONFIG_FILE_CLUSTER_2"

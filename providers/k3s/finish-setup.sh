eb09aa710767fa369fa15627bc2282f104abb0e1
Benedict Weis <47110382+benedictweis@users.noreply.github.com>
2025-07-09 16:28:10 +0200

    refinement

--- providers/k3s/finish-setup.sh
+++ providers/k3s/finish-setup.sh
@@ -13,7 +13,7 @@ KUBECONFIG_FILE_CLUSTER_2="../../kubeconfig_instance_2.yaml"
 echo $CLUSTER_1_NAME >"../../$CONTEXT_1_FILE"
 echo $CLUSTER_2_NAME >"../../$CONTEXT_2_FILE"
 
-yq -i '
+yq -y -i '
     .clusters[] |= 
         if .name == "default" then .name = env.CLUSTER_1_NAME | .cluster else . end |
     .contexts[] |= 
@@ -23,7 +23,7 @@ yq -i '
     .current-context = env.CLUSTER_1_NAME
 ' "$KUBECONFIG_FILE_CLUSTER_1"
 
-yq -i '
+yq -y -i '
     .clusters[] |= 
         if .name == "default" then .name = env.CLUSTER_2_NAME | .cluster else . end |
     .contexts[] |= 
@@ -38,3 +38,4 @@ yq -i ".clusters[0].name = \"$CLUSTER_2_NAME\"" "$KUBECONFIG_FILE_CLUSTER_2"
 
 export KUBECONFIG="$KUBECONFIG_FILE_CLUSTER_1:$KUBECONFIG_FILE_CLUSTER_2"
 kubectl config view --flatten >"../../$KUBECONFIG_FILE"
+export KUBECONFIG="../../$KUBECONFIG_FILE"

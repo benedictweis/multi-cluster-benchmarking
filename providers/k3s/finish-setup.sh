0d27b19e28274693e51f7823033e225663c3ff9c
Benedict Weis <47110382+benedictweis@users.noreply.github.com>
2025-07-09 16:24:42 +0200

    refinement

--- providers/k3s/finish-setup.sh
+++ providers/k3s/finish-setup.sh
@@ -13,8 +13,28 @@ KUBECONFIG_FILE_CLUSTER_2="../../kubeconfig_instance_2.yaml"
 echo $CLUSTER_1_NAME >"../../$CONTEXT_1_FILE"
 echo $CLUSTER_2_NAME >"../../$CONTEXT_2_FILE"
 
-kubectl --kubeconfig="$KUBECONFIG_FILE_CLUSTER_1" config rename-context "$(kubectl --kubeconfig="$KUBECONFIG_FILE_CLUSTER_1" config current-context)" "$CLUSTER_1_NAME"
-kubectl --kubeconfig="$KUBECONFIG_FILE_CLUSTER_2" config rename-context "$(kubectl --kubeconfig="$KUBECONFIG_FILE_CLUSTER_2" config current-context)" "$CLUSTER_2_NAME"
+yq -i '
+    .clusters[] |= 
+        if .name == "default" then .name = env.CLUSTER_1_NAME | .cluster else . end |
+    .contexts[] |= 
+        if .name == "default" then .name = env.CLUSTER_1_NAME | .context.cluster = env.CLUSTER_1_NAME | .context.user = "user-1" else . end |
+    .users[] |= 
+        if .name == "default" then .name = "user-1" else . end |
+    .current-context = env.CLUSTER_1_NAME
+' "$KUBECONFIG_FILE_CLUSTER_1"
+
+yq -i '
+    .clusters[] |= 
+        if .name == "default" then .name = env.CLUSTER_2_NAME | .cluster else . end |
+    .contexts[] |= 
+        if .name == "default" then .name = env.CLUSTER_2_NAME | .context.cluster = env.CLUSTER_2_NAME | .context.user = "user-2" else . end |
+    .users[] |= 
+        if .name == "default" then .name = "user-2" else . end |
+    .current-context = env.CLUSTER_2_NAME
+' "$KUBECONFIG_FILE_CLUSTER_2"
+
+yq -i ".clusters[0].name = \"$CLUSTER_1_NAME\"" "$KUBECONFIG_FILE_CLUSTER_1"
+yq -i ".clusters[0].name = \"$CLUSTER_2_NAME\"" "$KUBECONFIG_FILE_CLUSTER_2"
 
 export KUBECONFIG="$KUBECONFIG_FILE_CLUSTER_1:$KUBECONFIG_FILE_CLUSTER_2"
 kubectl config view --flatten >"../../$KUBECONFIG_FILE"

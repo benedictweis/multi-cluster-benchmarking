0b257ab9fa39774c6618b843bbd6c61ef20dc835
Benedict Weis <47110382+benedictweis@users.noreply.github.com>
2025-07-10 10:47:02 +0200

    cilium rework

--- approaches/cilium/uninstall.sh
+++ approaches/cilium/uninstall.sh
@@ -9,5 +9,5 @@ CLUSTER_1_CONTEXT=$(cat "../../$CONTEXT_1_FILE")
 CLUSTER_2_CONTEXT=$(cat "../../$CONTEXT_2_FILE")
 
 approachinfo "Uninstalling Cilium"
-helm upgrade --install --reset-values --version 1.17.5 -n kube-system cilium cilium/cilium --set ipam.mode=kubernetes --kube-context "${CLUSTER_1_CONTEXT}"
-helm upgrade --install --reset-values --version 1.17.5 -n kube-system cilium cilium/cilium --set ipam.mode=kubernetes --kube-context "${CLUSTER_2_CONTEXT}"
+helm upgrade --install --reset-values --version 1.17.5 -n kube-system cilium cilium/cilium --kube-context "${CLUSTER_1_CONTEXT}"
+helm upgrade --install --reset-values --version 1.17.5 -n kube-system cilium cilium/cilium --kube-context "${CLUSTER_2_CONTEXT}"

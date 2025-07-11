d1cb4bc2584fa3e199e9c2c96b82e053494382d1
Benedict Weis <47110382+benedictweis@users.noreply.github.com>
2025-07-11 07:51:02 +0200

    metrics server in k3s

--- providers/k3s/create.sh
+++ providers/k3s/create.sh
@@ -6,12 +6,6 @@ source ../../config.cfg
 source ../../helper.sh
 
 TERRAFORM_DIR="../../infra/aws-dual-ec2"
-
-info "[$PROVIDER] Running 'tofu apply' in $TERRAFORM_DIR"
-pushd "$TERRAFORM_DIR" > /dev/null
-tofu apply -auto-approve
-popd > /dev/null
-
 INVENTORY_FILE="$TERRAFORM_DIR/hosts.ini"
 INSTANCE_VARS_FILE="$TERRAFORM_DIR/vars.yaml"
 USERNAME="admin"

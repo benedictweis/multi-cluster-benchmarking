a11cceb860dc1e85577f0f10a9417ac64775efbc
Benedict Weis <47110382+benedictweis@users.noreply.github.com>
2025-07-17 12:54:15 +0200

    do not restart all cilium pods

--- approaches/cilium/uninstall.sh
+++ approaches/cilium/uninstall.sh
@@ -20,10 +20,6 @@ helm upgrade --install --reset-values --version 1.17.5 -n kube-system cilium cil
         --set operator.replicas=1 \
         --kube-context "${CLUSTER_2_CONTEXT}" 
 
-approachinfo "Restarting all Cilium pods"
-kubectl --context="${CLUSTER_1_CONTEXT}" -n kube-system delete pod -l "app.kubernetes.io/part-of=cilium"
-kubectl --context="${CLUSTER_2_CONTEXT}" -n kube-system delete pod -l "app.kubernetes.io/part-of=cilium"
-
 approachinfo "Waiting for cilium to be ready"
 cilium status --context "${CLUSTER_1_CONTEXT}" --wait
 cilium status --context "${CLUSTER_2_CONTEXT}" --wait
